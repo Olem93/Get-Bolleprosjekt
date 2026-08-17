97a5fc3150f34888ed3c0e0a6cbea5f611ebc4df
Ole Martin Øverdal <ole.overdal@gmail.com>
2026-08-17 11:27:46 +0200

    Re-implementation of files in main

---
+++ script.js
@@ -0,0 +1,9 @@
+        function Purchased() { 
+            document.getElementById("buyButton").innerHTML = "Lagt til handekurv";
+            
+       }
+
+        function Purchased2() { 
+            document.getElementById("buyButton2").innerHTML = "Lagt til handekurv";
+            
+       } 
