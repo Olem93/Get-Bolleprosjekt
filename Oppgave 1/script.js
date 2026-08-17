a6873130d9796a01a0c512d368787e1d46170e2c
Ole Martin Øverdal <ole.overdal@gmail.com>
2026-08-17 11:28:47 +0200

    Delete Oppgave 1 directory

--- Oppgave 1/script.js
+++
@@ -1,9 +0,0 @@
-        function Purchased() { 
-            document.getElementById("buyButton").innerHTML = "Lagt til handekurv";
-            
-       }
-
-        function Purchased2() { 
-            document.getElementById("buyButton2").innerHTML = "Lagt til handekurv";
-            
-       } 
